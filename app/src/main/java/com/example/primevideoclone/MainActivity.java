9ffb4b2e279f3b50488d96e66baedad9bcd7b6da
Hiep Do Hoang <flasza20@gmail.com>
2022-05-02 09:44:10 +0700

    Setup auto slider and indicator

--- app/src/main/java/com/example/primevideoclone/MainActivity.java
+++ app/src/main/java/com/example/primevideoclone/MainActivity.java
@@ -11,30 +11,104 @@ import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     BannerMoviesPagerAdapter bannerMoviesPagerAdapter;
-    TabLayout tabLayout;
+    TabLayout indicatorTab, categoryTab;
     ViewPager bannerMoviesViewPager;
-    List<BannerMovies> bannerMoviesList;
+    List<BannerMovies> homeBannerList;
+    List<BannerMovies> tvShowBannerList;
+    List<BannerMovies> movieBannerList;
+    List<BannerMovies> kidsBannerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bannerMoviesList = new ArrayList<>();
-        bannerMoviesList.add(new BannerMovies(1, "Hai Phuong", "https://i.postimg.cc/76Hzkv97/banner1.jpg", ""));
-        bannerMoviesList.add(new BannerMovies(2, "Bo Gia", "https://i.postimg.cc/prgyy3fs/banner2.jpg", ""));
+        indicatorTab = findViewById(R.id.tab_indicator);
+        categoryTab = findViewById(R.id.tabLayout);
 
-        setBannerMoviesPagerAdapter(bannerMoviesList);
+        homeBannerList = new ArrayList<>();
+        homeBannerList.add(new BannerMovies(1, "Hai Phuong", "https://i.postimg.cc/76Hzkv97/banner1.jpg", ""));
+        homeBannerList.add(new BannerMovies(2, "Bo Gia", "https://i.postimg.cc/prgyy3fs/banner2.jpg", ""));
+        homeBannerList.add(new BannerMovies(3, "Fast & Furious", "https://i.postimg.cc/hhRBStKt/banner3.jpg", ""));
+        homeBannerList.add(new BannerMovies(4, "Doctor Strange", "https://i.postimg.cc/ydwdZB7w/banner4.jpg", ""));
+
+        tvShowBannerList = new ArrayList<>();
+        tvShowBannerList.add(new BannerMovies(1, "RAPVIET", "https://i.postimg.cc/LXVCq4ks/banner5.jpg", ""));
+        tvShowBannerList.add(new BannerMovies(2, "On Gioi Cau Day Roi", "https://i.postimg.cc/7Z7NGYwy/banner6.jpg", ""));
+
+        movieBannerList = new ArrayList<>();
+        movieBannerList.add(new BannerMovies(1, "A Beautiful Day", "https://i.postimg.cc/tT5yNxGQ/banner7.jpg", ""));
+        movieBannerList.add(new BannerMovies(2, "Crash Landing On You", "https://i.postimg.cc/mgMvSH7X/banner8.jpg", ""));
+
+        kidsBannerList = new ArrayList<>();
+        kidsBannerList.add(new BannerMovies(1, "BoBoiBoy", "https://i.postimg.cc/BnB00Ft8/banner9.jpg", ""));
+        kidsBannerList.add(new BannerMovies(1, "Phineas and Ferb", "https://i.postimg.cc/CKvpZVGS/banner10.jpg", ""));
+
+        //Default tab
+        setBannerMoviesPagerAdapter(homeBannerList);
+
+        //Selected category
+        categoryTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 1:
+                        setBannerMoviesPagerAdapter(tvShowBannerList);
+                        return;
+                    case 2:
+                        setBannerMoviesPagerAdapter(movieBannerList);
+                        return;
+                    case 3:
+                        setBannerMoviesPagerAdapter(kidsBannerList);
+                        return;
+                    default:
+                        setBannerMoviesPagerAdapter(homeBannerList);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
     private void setBannerMoviesPagerAdapter(List<BannerMovies> bannerMoviesList) {
         bannerMoviesViewPager = findViewById(R.id.banner_viewPager);
         bannerMoviesPagerAdapter = new BannerMoviesPagerAdapter(this, bannerMoviesList);
         bannerMoviesViewPager.setAdapter(bannerMoviesPagerAdapter);
+        indicatorTab.setupWithViewPager(bannerMoviesViewPager);
+
+        Timer sliderTimer = new Timer();
+        sliderTimer.scheduleAtFixedRate(new AutoSlider(), 4000, 6000);
+        indicatorTab.setupWithViewPager(bannerMoviesViewPager, true);
     }
 
+    class AutoSlider extends TimerTask {
+        @Override
+        public void run() {
+
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(bannerMoviesViewPager.getCurrentItem() < homeBannerList.size() - 1){
+                        bannerMoviesViewPager.setCurrentItem(bannerMoviesViewPager.getCurrentItem() + 1);
+                    }
+                    else{ bannerMoviesViewPager.setCurrentItem(0);}
+                }
+            });
+
+        }
+    }
 }
